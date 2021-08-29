8b3bf92336d6e4641f2293f037104f1de4f34976
Bert Peng <13521265217@163.com>
2021-08-29 23:55:19 +0800

    08292355

--- hello.py
+++ hello.py
@@ -25,12 +25,14 @@ GTM_VS_RE_STR = config.get('GTM', 'GTM_VS_RE_STR')
 GTM_SERVER_RE_STR = config.get('GTM', 'GTM_SERVER_RE_STR')
 GTM_MEMBER_RE_STR = config.get('GTM', 'GTM_MEMBER_RE_STR')
 GTM_POOL_RE_STR = config.get('GTM', 'GTM_POOL_RE_STR')
+GTM_WIDEIP_POOLS_RE_STR = config.get('GTM', 'GTM_WIDEIP_POOLS_RE_STR')
 GTM_WIDEIP_RE_STR = config.get('GTM', 'GTM_WIDEIP_RE_STR')
 
 vs_pattern = re.compile(GTM_VS_RE_STR, re.MULTILINE)
 server_pattern = re.compile(GTM_SERVER_RE_STR, re.MULTILINE)
 member_pattern = re.compile(GTM_MEMBER_RE_STR, re.MULTILINE)
 pool_pattern = re.compile(GTM_POOL_RE_STR, re.MULTILINE)
+wideip_pools_pattern = re.compile(GTM_WIDEIP_POOLS_RE_STR, re.MULTILINE)
 wideip_pattern = re.compile(GTM_WIDEIP_RE_STR, re.MULTILINE)
 
 ports_dir = json.loads(port_json_str)
@@ -46,7 +48,6 @@ pool_mx_data_v4 = {}
 pool_aaaa_data_v6 = {}
 pool_cname_data_v6 = {}
 pool_mx_data_v6 = {}
-server_ip_data_v4_new = {}
 
 
 def servers_format(vs_info, server_name, server_ip, server_status,version):
@@ -91,7 +92,7 @@ def get_gtm_servers(version):
     elif version == 'v6':
         gtm_servers = server_pattern.findall(ipv6_gtm_config)
     get_gtm_servers_rt = {}
-    print(len(list(gtm_servers)))
+
     for item in gtm_servers:
         get_gtm_servers_rt['server_name'] = item[0].strip()
         get_gtm_servers_rt['server_ip'] = item[1].strip()
@@ -107,7 +108,7 @@ def get_gtm_servers(version):
 def members_format(members_info, pool_name, pool_type, pool_status,version):
     pool_members = []
     members_infos = member_pattern.findall(members_info)
-    ips = ''
+    ips = []
     for member in members_infos:
         pool_member = {}
         server_vs = member[0].strip()
@@ -122,11 +123,11 @@ def members_format(members_info, pool_name, pool_type, pool_status,version):
             vs = spl[1]
             if member_status == 'enabled':
                 if pool_type == 'a' and server in server_ip_data_v4.keys():
-                    ips = ips + '\n' + server_ip_data_v4[server]
+                    ips.append(server_ip_data_v4[server])
                 elif pool_type == 'aaaa' and server in server_ip_data_v6.keys():
-                    ips = ips + '\n' + server_ip_data_v6[server]
+                    ips.append(server_ip_data_v6[server])
         elif pool_type == 'cname' or pool_type == 'mx':
-                ips = ips + '\n' + server_vs + '_' + member_status
+                ips.append(server_vs + '_' + member_status)
 
         pool_member['server_vs'] = server_vs
         pool_member['server'] = server
@@ -139,18 +140,18 @@ def members_format(members_info, pool_name, pool_type, pool_status,version):
     if pool_status == 'enabled':
         if version == 'v4':
             if pool_type == 'a':
-                pool_a_data_v4[pool_name] = ips.strip('\n')
+                pool_a_data_v4[pool_name] = ips
             elif pool_type == 'cname':
-                pool_cname_data_v4[pool_name] = ips.strip('\n')
+                pool_cname_data_v4[pool_name] = ips
             elif pool_type == 'mx':
-                pool_mx_data_v4[pool_name] = ips.strip('\n')
+                pool_mx_data_v4[pool_name] = ips
         if version == 'v6':
             if pool_type == 'aaaa':
-                pool_aaaa_data_v6[pool_name] = ips.strip('\n')
+                pool_aaaa_data_v6[pool_name] = ips
             elif pool_type == 'cname':
-                pool_cname_data_v6[pool_name] = ips.strip('\n')
+                pool_cname_data_v6[pool_name] = ips
             elif pool_type == 'mx':
-                pool_mx_data_v6[pool_name] = ips.strip('\n')
+                pool_mx_data_v6[pool_name] = ips
 
     return pool_members
 
@@ -161,8 +162,6 @@ def get_gtm_pools(version):
         gtm_pools = pool_pattern.findall(ipv4_gtm_config)
     elif version == 'v6':
         gtm_pools = pool_pattern.findall(ipv6_gtm_config)
-
-    # print(len(gtm_pools))
 
     gtm_pools_ret = []
     for item in gtm_pools:
@@ -179,6 +178,59 @@ def get_gtm_pools(version):
     return gtm_pools_ret
 
 
+def wideip_pools_format(pools_str, wideip_type, version):
+    wideip_pools_ret = []
+    wideip_pools = wideip_pools_pattern.findall(pools_str)
+    for item in wideip_pools:
+        pool = {}
+        ips = []
+        name = item[0].strip()
+        pool['name'] = name
+        pool['order'] = item[1].strip()
+        pool['ratio'] = item[2].strip()
+        if version == 'v4':
+            if wideip_type == 'a' and name in pool_a_data_v4.keys():
+                ips = pool_a_data_v4[name]
+            if wideip_type == 'cname' and name in pool_cname_data_v4.keys():
+                ips = pool_cname_data_v4[name]
+            if wideip_type == 'mx' and name in pool_mx_data_v4.keys():
+                ips = pool_mx_data_v4[name]
+        elif version == 'v6':
+            if wideip_type == 'aaaa' and name in pool_aaaa_data_v6.keys():
+                ips = pool_aaaa_data_v6[name]
+            if wideip_type == 'cname' and name in pool_cname_data_v6.keys():
+                ips = pool_cname_data_v6[name]
+            if wideip_type == 'mx' and name in pool_mx_data_v6.keys():
+                ips = pool_mx_data_v6[name]
+        pool['ips'] = ips
+        wideip_pools_ret.append(pool)
+
+    return wideip_pools_ret
+
+def wideip_pools_cname_format(pools_cname_str, version):
+    wideip_pools_cname_ret = []
+    wideip_pools_cname = wideip_pools_pattern.findall(pools_cname_str)
+    for item in wideip_pools_cname:
+        cname_pool = {}
+        domains = []
+        name = item[0].strip()
+        cname_pool['name'] = name
+        cname_pool['order'] = item[1].strip()
+        cname_pool['ratio'] = item[2].strip()
+        if version == 'v4':
+            if name in pool_cname_data_v4.keys():
+                domains = pool_cname_data_v4[name]
+        elif version == 'v6':
+            if name in pool_cname_data_v6.keys():
+                domains = pool_cname_data_v6[name]
+
+        cname_pool['domains'] = domains
+        wideip_pools_cname_ret.append(cname_pool)
+
+    return wideip_pools_cname_ret
+
+
+
 def get_gtm_wideip(version):
     gtm_wideip = []
     if version == 'v4':
@@ -191,24 +243,157 @@ def get_gtm_wideip(version):
         wideip['type'] = item[0].strip()
         wideip['wideip'] = item[1].strip()
         wideip['status'] = item[2].strip()
-        wideip['pool-lb-mode'] = item[3].strip()
-        wideip['pools'] = item[4].strip()
-        wideip['pools-cname'] = item[6].strip()
+        wideip['pool_lb_mode'] = item[3].strip()
+        wideip['pools'] = wideip_pools_format(item[4].strip(), item[0].strip(), version)
+        wideip['pools_cname'] = wideip_pools_cname_format(item[5].strip(), version)
         gtm_wideip_ret.append(wideip)
     return gtm_wideip_ret
 
 
 def main():
-    # get_gtm_servers('v6')
-    # get_gtm_servers('v6')
-    # get_gtm_pools('v6')
-    # poolconfig.close()
-    # get_gtm_pools('v6')
-    it1 = get_gtm_wideip('v4')
-    it2 = get_gtm_wideip('v6')
-    print(len(list(it1)))
-    print(len(list(it2)))
+    get_gtm_servers('v4')
+    get_gtm_servers('v6')
+    get_gtm_pools('v4')
+    get_gtm_pools('v6')
+    ipv4_wideips = get_gtm_wideip('v4')
+    ipv6_wideips = get_gtm_wideip('v6')
+    result_dir = {}
+
+    for wideip in ipv4_wideips:
+        if wideip['type'] == 'a' or wideip['type'] == 'mx':
+            wideip_str = wideip['wideip']
+            if wideip_str not in result_dir.keys():
+                records = [''] * 7
+                result_dir[wideip_str] = records
+
+            pools = wideip['pools']
+            ips = []
+            a_order = 99999
+            a_order_pool = ''
+            for pool in pools:
+                ips.append(pool['ips'])
+                pool_order = int(pool['order'])
+                if pool_order <= a_order:
+                    a_order = pool_order
+                    a_order_pool = pool['name']
+
+            pools_cname = wideip['pools_cname']
+            domains = []
+            cname_order = 99999
+            cname_order_domain_ga = ''
+            cname_order_domain_top = ''
+            for pool_cname in pools_cname:
+                domas = pool_cname['domains']
+                cnames = ''
+                for item in domas:
+                    it = re.split('_', item)
+                    cname = it[0]
+                    domains.append(cname)
+                    cnames = cname + '\n' + cnames
+                    if it[1].strip() == 'enabled':
+                        cname_order_domain_top = cname
+
+                    pool_order = int(pool_cname['order'])
+                    if pool_order <= cname_order:
+                        cname_order = pool_order
+                        cname_order_domain_ga = cnames.strip()
+
+            ips_list = []
+            for ip in ips:
+                ips_list = ips_list + ip
+            ips_list = list(set(ips_list))
+            ips_str = '\n'.join(ips_list)
+            if wideip['type'] == 'mx':
+                ips_str = ips_str.replace('_enabled','')
+
+            domains_list = list(set(domains))
+            domains_str = '\n'.join(domains_list)
+
+            if wideip['type'] == 'a':
+                result_dir[wideip_str][0] = ips_str
+                result_dir[wideip_str][2] = domains_str
+                pool_lb_mode = wideip['pool_lb_mode'].strip()
+                if cname_order_domain_ga != '' and pool_lb_mode == 'global-availability':
+                    if cname_order < a_order:
+                        result_dir[wideip_str][4] = cname_order_domain_ga
+                    else:
+                        result_dir[wideip_str][4] = a_order_pool
+                elif cname_order_domain_top != '' and pool_lb_mode == 'topology':
+                    result_dir[wideip_str][4] = cname_order_domain_top
+
+            elif wideip['type'] == 'mx':
+                result_dir[wideip_str][6] = ips_str
+
+
+    for wideip in ipv6_wideips:
+
+        if wideip['type'] == 'aaaa':
+            wideip_str = wideip['wideip']
+            if wideip_str not in result_dir.keys():
+                records = [''] * 7
+                result_dir[wideip_str] = records
+
+            pools = wideip['pools']
+            ips = []
+            a_order = 99999
+            a_order_pool = ''
+            for pool in pools:
+                ips.append(pool['ips'])
+                pool_order = int(pool['order'])
+                if pool_order <= a_order:
+                    a_order = pool_order
+                    a_order_pool = pool['name']
+
+            pools_cname = wideip['pools_cname']
+            domains = []
+            cname_order = 99999
+            cname_order_domain_ga = ''
+            cname_order_domain_top = ''
+            for pool_cname in pools_cname:
+                domas = pool_cname['domains']
+                cnames = ''
+                for item in domas:
+                    it = re.split('_', item)
+                    cname = it[0]
+                    cnames = cname + '\n' + cnames
+                    domains.append(cname)
+                    if it[1].strip() == 'enabled':
+                        cname_order_domain_top = cname
+
+                pool_order = int(pool_cname['order'])
+                if pool_order <= cname_order:
+                    cname_order = pool_order
+                    cname_order_domain_ga = cnames.strip()
+
+            ips_list = []
+            for ip in ips:
+                ips_list = ips_list + ip
+            ips_list = list(set(ips_list))
+            ips_str = '\n'.join(ips_list)
+
+
+            domains_list = list(set(domains))
+            domains_str = '\n'.join(domains_list)
+
+            result_dir[wideip_str][1] = ips_str
+            result_dir[wideip_str][3] = domains_str
+            pool_lb_mode = wideip['pool_lb_mode'].strip()
+            if cname_order_domain_ga != '' and pool_lb_mode == 'global-availability':
+                if cname_order < a_order:
+                    result_dir[wideip_str][5] = cname_order_domain_ga
+                else:
+                    result_dir[wideip_str][5] = a_order_pool
+            elif cname_order_domain_top != '' and pool_lb_mode == 'topology':
+                result_dir[wideip_str][5] = cname_order_domain_top
+
+    print(result_dir['ssz.ccb.com'])
+
+
     #
+    # for wideip in ipv6_wideips:
+    #     if wideip['type'] == 'aaaa':
+    #         print(wideip)
+
     # for v in server_ip_data_v4_new.keys():
     #     poolconfig.write(v + '\n')
     # print(pool_cname_data_v4['pool_CDNS_mmerchant.ccb.com'])
